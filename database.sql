072cf42b4fb364af59d37f9c5945c12fb9975797
Esteban <59137626+EstebanBaron@users.noreply.github.com>
2020-11-08 15:40:32 +0100

    database terminé
    premier test de database terminé

--- database.sql
+++ database.sql
@@ -1,4 +1,10 @@
---rajouter les relations pere-fils--;
+DROP TABLE IF EXISTS organisateur CASCADE; 
+DROP TABLE IF EXISTS evenement CASCADE; 
+DROP TABLE IF EXISTS tournois CASCADE; 
+DROP TABLE IF EXISTS terrain CASCADE; 
+DROP TABLE IF EXISTS dispose CASCADE;
+DROP TABLE IF EXISTS equipe CASCADE;
+DROP TABLE IF EXISTS joueur CASCADE; 
 
 CREATE TABLE organisateur (
     numOrganisateur integer CONSTRAINT organisateur_PK PRIMARY KEY,
@@ -12,27 +18,43 @@ CREATE TABLE evenement (
     numEvenement integer CONSTRAINT evenement_PK PRIMARY KEY,
     lieu VARCHAR(20),
     dateEvenement DATE,
-    typeJeu integer CHECK(typeJeu BETWEEN 1 AND 11)
+    typeJeu integer CHECK(typeJeu BETWEEN 1 AND 11),
+    numOrga integer NOT NULL,
+    CONSTRAINT evenement_FK FOREIGN KEY (numOrga) REFERENCES organisateur(numOrganisateur) ON DELETE CASCADE
 );
 
 CREATE TABLE tournois (
     numTournois integer CONSTRAINT tournois_PK PRIMARY KEY,
-    nom VARCHAR(15) NOT NULL
+    nom VARCHAR(15) NOT NULL,
+    numEvenement integer NOT NULL,
+    CONSTRAINT tournois_FK FOREIGN KEY (numEvenement) REFERENCES evenement(numEvenement) ON DELETE CASCADE
 );
 
 CREATE TABLE terrain (
     numTerrain integer CONSTRAINT terrain_PK PRIMARY KEY,
-    sport VARCHAR(10) NOT NULL CHECK(IN ('foot', 'rugby', 'basket', 'volley', 'petanque', 'tennis'))
+    sport VARCHAR(10) NOT NULL CHECK(sport IN ('foot', 'rugby', 'basket', 'volley', 'petanque', 'tennis'))
+);
+
+CREATE TABLE dispose (
+    numEvenement integer NOT NULL,
+    CONSTRAINT disposeEvenement_FK FOREIGN KEY (numEvenement) REFERENCES evenement(numEvenement),
+    numTerrain integer NOT NULL,
+    CONSTRAINT disposeTerrain_FK FOREIGN KEY (numTerrain) REFERENCES terrain(numTerrain),
+    CONSTRAINT dispose_PK PRIMARY KEY (numEvenement, numTerrain)
 );
 
 CREATE TABLE equipe (
     nom VARCHAR(15) CONSTRAINT equipe_PK PRIMARY KEY,
-    club VARCHAR(15)
+    club VARCHAR(15),
+    numTournois integer NOT NULL,
+    CONSTRAINT equipe_FK FOREIGN KEY (numTournois) REFERENCES tournois(numTournois) ON DELETE CASCADE
 );
 
 CREATE TABLE joueur (
     numJoueur integer CONSTRAINT joueur_PK PRIMARY KEY,
     nom VARCHAR(15),
     prenom VARCHAR(15),
-    niveau integer NOT NULL CHECK(niveau BETWEEN 1 AND 7)
+    niveau integer NOT NULL CHECK(niveau BETWEEN 1 AND 7),
+    nomEquipe VARCHAR(15) NOT NULL,
+    CONSTRAINT joueur_FK FOREIGN KEY (nomEquipe) REFERENCES equipe(nom) ON DELETE CASCADE
 );
