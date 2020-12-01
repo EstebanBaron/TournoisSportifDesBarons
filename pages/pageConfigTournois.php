9b0aed05e1a25870cd689e864815084167275a5a
Esteban <59137626+EstebanBaron@users.noreply.github.com>
2020-12-01 17:43:47 +0100

    Modif pageConfigTournois

--- pages/pageConfigTournois.php
+++ pages/pageConfigTournois.php
@@ -8,79 +8,83 @@ session_start();
   </head>
   <body>
   <?php 
-    if(isset($_POST["numtournois"]))
-    {
-      try{
-        $dbh = new PDO("pgsql:dbname=postgres;host=localhost;user=postgres;password=carpate3433;options='--client_encoding=UTF8'");
-        $tournois = $dbh->query('SELECT * FROM tournois');
-        $nomTournois = NULL; // la table de tournois selectionné
-        if($tournois)
-        {
-            foreach($tournois as $row)
-            {
-                if($row['numtournois'] == $_POST['numtournois'])
-                {
-                    $nomTournois = $row['nom'];
-                }
-            }
-        }
-        else 
-        {
-            echo "Erreur, les données de la base n'ont pas pu être récupérées !"; 
-        }
+  if(isset($_POST["numtournois"]))
+  {
+    try{
+      $dbh = new PDO("pgsql:dbname=postgres;host=localhost;user=postgres;password=carpate3433;options='--client_encoding=UTF8'");
+      $tournois = $dbh->query('SELECT * FROM tournois');
+      $nomTournois = NULL; // la table de tournois selectionné
+      if($tournois)
+      {
+          foreach($tournois as $row)
+          {
+              if($row['numtournois'] == $_POST['numtournois'])
+              {
+                  $nomTournois = $row['nom'];
+              }
+          }
+      }
+      else 
+      {
+          echo "Erreur, les données de la base n'ont pas pu être récupérées !"; 
+      }
     } catch (PDOException $e)
     {
         print "Erreur ! : " . $e->getMessage() . "<br>";
     }
-    
-  
   ?>
   <h1>Tournois : <?php echo '"' . $nomTournois . '"';  ?> </h1>
   <?php
-      $equipeOk = false;
-      $cloture = false;
-      if(!$equipeOk || !$cloture)
-      {
-        echo '<form methode="post" action = "pageEquipe.php"><br>';
-        echo '<input type="submit" value = "' . htmlspecialchars($_POST["numtournois"]) . '" >';
-        echo '</form>';
-        try{
-          $dbh = new PDO("pgsql:dbname=postgres;host=localhost;user=postgres;password=carpate3433;options='--client_encoding=UTF8'");
-          $tournois = $dbh->query('SELECT numTournois , count(*) AS nbEquipe FROM tournois t, equipe e WHERE e.numTournois = t.numTournois GROUP BY t.numTournois');
-            
-          if($tournois)
+    $equipeOk = false;
+    $cloture = false;
+    if(!$equipeOk || !$cloture)
+    {
+      echo '<form method="post" action="pageEquipe.php"><br>';
+      echo '<input type="hidden" value="' . htmlspecialchars($_POST["numtournois"]) . '" >';
+      echo '<input type="submit" value="Ajouter des équipes">';
+      echo '</form>';
+      try{
+        $dbh = new PDO("pgsql:dbname=postgres;host=localhost;user=postgres;password=carpate3433;options='--client_encoding=UTF8'");
+        $tournois = $dbh->query('SELECT t.numtournois, count(*) AS nbequipe FROM tournois t, equipe e WHERE e.numtournois = t.numtournois GROUP BY t.numtournois');
+          
+        if($tournois)
+        {
+          foreach($tournois as $row)
           {
-            foreach($tournois as $row)
+            if($row['numtournois'] == $_POST['numtournois'])
             {
-              if($row['numTournois'] == $_POST['numtournois'] && $row['nbEquipe'] > 2)
+              if($row['nbequipe'] > 2) 
               {
                 $equipeOk = true;
-                echo '<button>colturer l\'ajout des équipes</button>'; //A TERMINER
+                echo '<button>cloturer l\'ajout des équipes</button>'; //A TERMINER
               }
-              else{
-              echo 'pas assez d\'équipe (minimum : 2)';
+              else
+              {
+                echo 'pas assez d\'équipe (minimum : 2)';
               }
             }
           }
-          else 
-          {
-            echo "Erreur, les données de la base n'ont pas pu être récupérées !"; 
-          }
-        } catch (PDOException $e)
-        {
-            print "Erreur ! : " . $e->getMessage() . "<br>";
         }
-      }
-      else($equipeOk && $cloture)
+        else 
+        {
+          echo "Erreur, les données de la base n'ont pas pu être récupérées !"; 
+        }
+      } catch (PDOException $e)
       {
-        echo '<form methode="post" action = "pageTournois.php"><br>';
-        echo '<input type="submit" value = "' . htmlspecialchars($_POST["numtournois"]) . '" >';
-        echo '</form>';
+          print "Erreur ! : " . $e->getMessage() . "<br>";
       }
+    }
     else
     {
-        echo 'Erreur pas de Tournois trouvé';
+      echo '<form method="post" action="pageTournois.php"><br>';
+      echo '<input type="submit" value = "' . htmlspecialchars($_POST["numtournois"]) . '" >';
+      echo '</form>';
     }
+  }
+  else
+  {
+      echo 'Erreur pas de Tournois trouvé';
+  }
   ?>
   </body>
 </html>
--- pages/pageCreationEvenement.php
+++ pages/pageCreationEvenement.php
@@ -66,63 +66,6 @@ session_start();
     <br><br>
     <input type="submit" value="Creer l'événement" name="creationevenement">
     <?php
-    if (isset($_POST['nom']) && isset($_POST['lieu']) && isset($_POST['dateevenement']) && isset($_POST['creationevenement'])) {
-        $identifiant = $_POST['identifiant'];
-        $mdp = $_POST['motdepasse'];
-        $confirmMDP = $_POST['confirmationMDP'];
-        if ($mdp !== $confirmMDP) {
-            echo "Erreur, les mots de passes sont incohérents !";
-        }
-        else {
-            try {
-                $dbh = new PDO("pgsql:dbname=postgres;host=localhost;user=postgres;password=carpate3433;options='--client_encoding=UTF8'");
-                $organisateur = $dbh->query('SELECT * from organisateur');
-                if ($organisateur) {
-                    $dejaUtilise = false;
-                    foreach($organisateur as $row) {
-                        foreach($row as $cle=>$value) {
-                            if($identifiant === $row['identifiant']) {
-                                echo "Erreur, nom d'utilisateur déjà utilisé !";
-                                $dejaUtilise = true;
-                            }
-                        }
-                    }
-                    if(!$dejaUtilise) {
-                        $sql = "INSERT INTO organisateur (identifiant, motdepasse, nom, prenom) VALUES (?, ?)";
-                        $stmt = $dbh->prepare($sql);
-                        //ajout utilisateur
-                        if(strlen($identifiant) <= 20 && strlen(md5($mdp)) <= 50) {
-                            if ($stmt->execute([$identifiant, md5($mdp)])) {
-                                $_SESSION['identifiant'] = $identifiant;
-                                echo "Enregistrement réussi. Bienvenue " . htmlspecialchars($_SESSION['identifiant']) . " !";
-                                ?>
-                                <br>
-                                <h4>Redirection en cours...</h4>
-                                <script type="text/javascript">
-                                    //permet d'attentre 3000ms (3s) avant d'aller sur pageAccueil.php
-                                    setTimeout(function() {window.location.href = "pageAccueil.php";}, 3000);
-                                </script>
-                                <?php
-                            }
-                            else {
-                                echo "Erreur d'execution de la requête préparé !";
-                            }
-                        }
-                        else {
-                            echo "Erreur, la taille des champs renseigné dépasse la limite autorisé !";
-                        }
-                    }
-                }
-                else {
-                    echo "Erreur, les données de la base n'ont pas pu être récupérées !"; 
-                }
-                $dbh = null;
-            } catch (PDOException $e) {
-                print "Erreur ! : " . $e->getMessage() . "<br>";
-                die();
-            }
-        }
-    }
     ?>
     <script src="autreElement.js"></script>
   </body>
