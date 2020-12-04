bb9d0e8cb2e2f4d4bcc981a797fd2a152e0102f9
julien Lebaron <julienlebaron@hotmail.com>
2020-12-04 10:29:31 +0100

    debut de la page tournois

--- pages/pageTournois.php
+++ pages/pageTournois.php
@@ -7,17 +7,43 @@ session_start();
     <title>Page Tournois</title>
   </head>
   <body>
-  <?php
+  <?php 
   if(isset($_POST["numtournois"]))
-  { 
-    
+  {
+    try{
+      $dbh = new PDO("pgsql:dbname=bddestebanjulien;host=localhost;user=bddestebanjulien;password=lesbarons;options='--client_encoding=UTF8'");
+      $tournois = $dbh->query('SELECT * FROM tournois');
+      $nomTournois = NULL; // la table de tournois selectionné
+      if($tournois)
+      {
+          foreach($tournois as $row)
+          {
+              if($row['numtournois'] == $_POST['numtournois'])
+              {
+                  $nomTournois = $row['nom'];
+              }
+          }
+      }
+      else 
+      {
+          echo "Erreur, les données de la base n'ont pas pu être récupérées !"; 
+      }
+    } catch (PDOException $e)
+    {
+        print "Erreur ! : " . $e->getMessage() . "<br>";
+    }
+  ?>
+  <h1>Tournois : <?php echo '"' . $nomTournois . '"';  ?> </h1>
+
+  <?php 
+  
+  
   }
   else
   {
       echo 'Erreur pas de Tournois trouvé';
   }
-    ?>
-  <h1>equipe :  </h1>
+  ?>
  
   </body>
 </html>
