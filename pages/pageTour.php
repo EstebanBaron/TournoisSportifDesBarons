0e1f12ed76404556a061983d9fc764395af5c9c3
julien Lebaron <julienlebaron@hotmail.com>
2020-12-05 17:02:33 +0100

    pagetour complété mais pas fini
    rajout de fct crepoulerandom
    et des input pour aller sur les pages suivantes

--- pages/pageTour.php
+++ pages/pageTour.php
@@ -1,5 +1,43 @@
 <?php
 session_start();
+
+function PGNiveaux($listeNiveaux)
+{
+    $pgn = 1;
+    for($i = 0 ; $i< count($listeNiveaux); $i++)
+    {
+        if($listeNiveaux[$i] > $pgn)
+        {
+            $pgn=$listeNiveaux[$i];
+        }
+    }
+    return $pgn;
+}
+
+function creePouleRandom($listeEquipe, $listeNiveaux, $nbPoules)
+{
+    $pouleRandom = array(array());// faire une fonction qui créé un tableau de tableau bisous
+    $pgn = PGNiveaux($listeNiveaux);
+    $listeN = $listeNiveaux;
+    for($i=0; $i<count($listeEquipe); $i++)
+    {
+        for($j=0; $j<count($listeEquipe); $j++)
+        {
+            if($listeN[$i] = $pgn)
+            {
+                array_push($pouleRandom[$i>$nbPoules ? ($i-$nbPoules) : $i],$listeEquipe[$i]);
+                $listeN[$i] = null;
+            }
+            else if($j++==count($listeEquipe))
+            {
+                $pgn--;
+            }
+        }
+        $pgn--;//pas sur
+    }
+
+    return $pouleRandom;
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,8 +46,7 @@ session_start();
     </head>
     <body>
         <?php 
-        echo $_POST['numtour'];
-        if(isset($_POST['numt'])) //erreur inconnue
+        if(isset($_POST['numt']) && isset($_POST['numtour']))
         {
             try{
             $dbh = new PDO("pgsql:dbname=bddestebanjulien;host=localhost;user=bddestebanjulien;password=lesbarons;options='--client_encoding=UTF8'");
@@ -19,7 +56,7 @@ session_start();
             {
                 foreach($tournois as $row)
                 {
-                    if($row['numtournois'] == $_POST['numtournois'])
+                    if($row['numtournois'] == $_POST['numt'])
                     {
                         $nomTournois = $row['nom'];
                     }
@@ -34,10 +71,18 @@ session_start();
                 print "Erreur ! : " . $e->getMessage() . "<br>";
             }
         ?>
-        <h1>Tournois : <?php echo '"' . $nomTournois . '"';  ?> <br> Tour n°: <?php echo '"' . $_POST["numtour"] . '"';?></h1>
+        <h1>Tournois : <?php echo $nomTournois;  ?> <br> Tour n°: <?php echo  $_POST["numtour"];?></h1>
 
+
+        <form method ="post" action="pageModifPoules.php">
+        <input type="submit" name="modifPoules" value="modifier les Poules">
+        </form>
+
+        <form method ="post" action="pagePoule.php">
+        <input type="submit" name="CommencerPoules" value="Commencer les Poules">
+        </form>
         <?php 
-        
+            print_r(creePouleRandom(array("eq1","eq2","eq3","eq4","eq5","eq6"),array(1,2,1,3,2,1),3));
         }
         else
         {
--- pages/pageTournois.php
+++ pages/pageTournois.php
@@ -69,7 +69,7 @@ function nbEquipeTourSuivant($nb)
       }
       else if($nbtour == $_SESSION["TourActuel" . $_POST["numtournois"]] )
       { 
-        echo '<form type="post" action="pageTour.php" >';
+        echo '<form method="post" action="pageTour.php" >';
         echo '<input type="hidden" name="numt" value="' . $_POST["numtournois"] . '" >';
         echo '<input type="hidden" name="numtour" value="' . $nbtour . '" >';
         echo '<input type="submit" name="boutonpoule" value="Poules" >';
