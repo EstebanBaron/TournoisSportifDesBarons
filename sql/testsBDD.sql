f9aacacb207472736ab87ad2dfa151c5a2113a49
julien Lebaron <julienlebaron@hotmail.com>
2020-12-13 17:13:18 +0100

    creation des schéma et maj des tests

--- sql/testsBDD.sql
+++ sql/testsBDD.sql
@@ -21,7 +21,7 @@ INSERT INTO organisateur VALUES('Julien', 'a64cd8062eaa4562c0ba463f2ee7c828');
 INSERT INTO evenement VALUES(01, 'event regional 1', 'Montpellier', '2020-11-9', 'Esteban');
 INSERT INTO evenement VALUES(02, 'event regional 2', 'Montpellier', '2020-11-10', 'Esteban');
 INSERT INTO evenement VALUES(03, 'event regional 3', 'Montpellier', '2020-11-11', 'Esteban');
-INSERT INTO evenement VALUES(04, 'compet pez', 'Pezenas', '2020-12-10', 'Julien');
+INSERT INTO evenement VALUES(04, 'competition pez', 'Pezenas', '2020-12-10', 'Julien');
 
 --Insertion dans tournois
 
@@ -47,7 +47,7 @@ INSERT INTO terrain VALUES(05, 'Basketball',04);
 
 --Insertion dans equipe
 
-INSERT INTO equipe VALUES('Les papis', 'PPP', 01);
+INSERT INTO equipe VALUES('Lespapis', 'PPP', 01);
 INSERT INTO equipe VALUES('NomEq1', 'nomClub1', 01);
 INSERT INTO equipe VALUES('NomEq2', 'nomClub2', 01);
 INSERT INTO equipe VALUES('NomEq10', 'nomClub10', 01);
@@ -56,14 +56,14 @@ INSERT INTO equipe VALUES('NomEq3', 'nomClub3', 02);
 INSERT INTO equipe VALUES('NomEq4', 'nomClub4', 02);
 INSERT INTO equipe VALUES('NomEq5', 'nomClub5', 03);
 INSERT INTO equipe VALUES('NomEq6', 'nomClub6', 03);
-INSERT INTO equipe VALUES('equipeEste', 'Les meilleurs', 04);
-INSERT INTO equipe VALUES('equipeNullos', 'Les Nullos', 04);
+INSERT INTO equipe VALUES('equipeMTP', 'MTP' , 04);
+INSERT INTO equipe VALUES('equipePSG', 'PSG', 04);
 
 --Insertion dans joueur
-INSERT INTO joueur VALUES(01, NULL, NULL, 7,'Les papis');
-INSERT INTO joueur VALUES(02, NULL, NULL, 6, 'Les papis');
-INSERT INTO joueur VALUES(03, NULL, NULL, 6, 'Les papis');
-INSERT INTO joueur VALUES(34, NULL, NULL, 1, 'Les papis');
+INSERT INTO joueur VALUES(01, NULL, NULL, 7,'Lespapis');
+INSERT INTO joueur VALUES(02, NULL, NULL, 6, 'Lespapis');
+INSERT INTO joueur VALUES(03, NULL, NULL, 6, 'Lespapis');
+INSERT INTO joueur VALUES(34, NULL, NULL, 1, 'Lespapis');
 INSERT INTO joueur VALUES(04, NULL, NULL, 7, 'NomEq1');
 INSERT INTO joueur VALUES(05, NULL, NULL, 7, 'NomEq1');
 INSERT INTO joueur VALUES(06, NULL, NULL, 7, 'NomEq1');
@@ -83,18 +83,18 @@ INSERT INTO joueur VALUES(19, NULL, NULL, 6, 'NomEq6');
 INSERT INTO joueur VALUES(20, NULL, NULL, 7, 'NomEq6');
 INSERT INTO joueur VALUES(21, NULL, NULL, 7, 'NomEq6');
 
-INSERT INTO joueur VALUES(22, 'BARON', 'Esteban', 7, 'equipeEste');
-INSERT INTO joueur VALUES(23, NULL, NULL, 2, 'equipeEste');
-INSERT INTO joueur VALUES(24, NULL, NULL, 2, 'equipeEste');
-INSERT INTO joueur VALUES(25, NULL, NULL, 3, 'equipeEste');
-INSERT INTO joueur VALUES(26, NULL, NULL, 3, 'equipeEste');
-INSERT INTO joueur VALUES(27, NULL, NULL, 2, 'equipeEste');
-INSERT INTO joueur VALUES(28, NULL, NULL, 1, 'equipeNullos');
-INSERT INTO joueur VALUES(29, NULL, NULL, 1, 'equipeNullos');
-INSERT INTO joueur VALUES(30, NULL, NULL, 1, 'equipeNullos');
-INSERT INTO joueur VALUES(31, NULL, NULL, 1, 'equipeNullos');
-INSERT INTO joueur VALUES(32, NULL, NULL, 1, 'equipeNullos');
-INSERT INTO joueur VALUES(33, NULL, NULL, 1, 'equipeNullos');
+INSERT INTO joueur VALUES(22, 'BARON', 'Esteban', 7, 'equipeMTP');
+INSERT INTO joueur VALUES(23, NULL, NULL, 2, 'equipeMTP');
+INSERT INTO joueur VALUES(24, NULL, NULL, 2, 'equipeMTP');
+INSERT INTO joueur VALUES(25, NULL, NULL, 3, 'equipeMTP');
+INSERT INTO joueur VALUES(26, NULL, NULL, 3, 'equipeMTP');
+INSERT INTO joueur VALUES(27, NULL, NULL, 2, 'equipeMTP');
+INSERT INTO joueur VALUES(28, NULL, NULL, 1, 'equipePSG');
+INSERT INTO joueur VALUES(29, NULL, NULL, 1, 'equipePSG');
+INSERT INTO joueur VALUES(30, NULL, NULL, 1, 'equipePSG');
+INSERT INTO joueur VALUES(31, NULL, NULL, 1, 'equipePSG');
+INSERT INTO joueur VALUES(32, NULL, NULL, 1, 'equipePSG');
+INSERT INTO joueur VALUES(33, NULL, NULL, 1, 'equipePSG');
 
 INSERT INTO joueur VALUES(35, NULL, NULL, 7, 'NomEq10');
 INSERT INTO joueur VALUES(36, NULL, NULL, 7, 'NomEq10');
@@ -107,10 +107,10 @@ INSERT INTO joueur VALUES(42, NULL, NULL, 7, 'NomEq11');
 
 --TEST DES CONTRAINTES
 
-
+/*
 INSERT INTO tournois VALUES(50, 'tournois 50','EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE',5, 01);
 INSERT INTO equipe VALUES('N om Eq 120', 'nomC', 03);
-/* 
+ 
 
 INSERT INTO terrain VALUES (01, '100m');
 
